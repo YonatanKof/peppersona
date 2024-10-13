4fb2e6a90bd58e882d79f7640d707e485585c16e
Yonatan Kof <kof@yonatankof.com>
2024-10-13 20:35:06 +0300

    feat: Add subTitle and roles to Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,27 +1,35 @@
 <script setup>
 const props = defineProps({
 	title: String,
+	subTitle: String,
 	descAi: String,
 	itemId: String,
 	imgSrc: String,
 	painPoints: Array,
 	unmetNeeds: Array,
 	goals: Array,
+	roles: Array,
 });
 import List from '../components/List.vue';
-
 </script>
 
 <template>
 	<article>
-		<span>
-			<img :src="imgSrc" alt="" v-if="imgSrc" />
-			<h3 id="title">{{ title }}</h3>
+		<span class="card-title">
+			<img :src="imgSrc" :alt="`Picture of ${title} the ${subTitle}`" v-if="imgSrc" />
+			<div class="titles">
+				<h2 id="title" v-if="title">{{ title }}</h2>
+				<h3 id="sub-title" v-if="subTitle">{{ subTitle }}</h3>
+			</div>
+		</span>
+		<span class="card-role">
+			<p>Role:</p>
+			<p class="role" v-if="roles" v-for="role in roles">{{ role }}</p>
 		</span>
 		<p v-if="descAi">{{ descAi }}</p>
-		<List color="orangered" listName="Unmet Needs" :list="unmetNeeds" />
-		<List color="darkred" listName="Pain Points" :list="painPoints" />
-		<List color="darkgreen" listName="Goals" :list="goals" />
+		<List color="orangered" darkColor="orange" listName="Unmet Needs" :list="unmetNeeds" />
+		<List color="crimson" darkColor="red" listName="Pain Points" :list="painPoints" />
+		<List color="darkgreen" darkColor="lightgreen" listName="Goals" :list="goals" />
 		<RouterLink :to="itemId">
 			<button>Read more about {{ title }}</button>
 		</RouterLink>
@@ -29,35 +37,55 @@ import List from '../components/List.vue';
 </template>
 
 <style>
-article {
-	border: 1px solid #ccc;
-	padding: 1rem;
-	display: flex;
-	flex-direction: column;
-	border-radius: 0.5rem;
-	height: min-content;
-}
-#title {
+#title,
+h3 {
 	margin: 0;
 }
-span {
+.titles {
+	display: flex;
+	flex-direction: column;
+	gap: 0.25rem;
+}
+.card-title {
 	display: flex;
 	justify-content: start;
 	align-items: center;
 	gap: 1rem;
 	margin-block-end: 1rem;
 }
+.card-role {
+	display: flex;
+	justify-content: start;
+	align-items: center;
+	gap: 0.5em;
+	margin-block-end: 0.5rem;
+}
 p {
 	margin-block-start: 0;
 	margin-block-end: 1rem;
 }
-img {
-	max-width: 5rem;
-	width: 100%;
+.role {
+	background-color: var(--color-system-invert-80);
 	border-radius: 0.25rem;
-	height: auto;
+	padding: 0.2rem 0.4rem;
+	font-size: 75%;
+	margin: unset;
+	color: var(--color-system-100);
 }
-button{
+.card-role > p:first-child {
+	margin: 0;
+	font-size: 75%;
+	color: var(--color-system-60);
+}
+
+img {
+	height: 5rem;
+	width: 5rem;
+	border-radius: 0.25rem;
+	object-fit: cover;
+}
+a > button {
 	width: 100%;
+	margin-block-start: 0.5rem;
 }
 </style>
