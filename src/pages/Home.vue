c5f0eb8335c7082f97405fe282c7e44c9fa945da
Yonatan Kof <kof@yonatankof.com>
2024-10-13 20:35:45 +0300

    feat: Improve Home.vue to display database title, description, and loading spinner

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Card from '../components/Card.vue';
+import Loader from '../components/Loader.vue';
 import { onMounted, ref } from 'vue';
 const itemData = ref([]);
 const dbData = ref([]);
+const isLoading = ref(true);
 
 const getData = async () => {
 	return fetch('/api/fetchNotion').then((res) => res.json());
@@ -12,34 +14,46 @@ onMounted(() => {
 	getData().then((data) => {
 		itemData.value = data.response.results;
 		dbData.value = data?.responseDbInfo;
+		isLoading.value = false;
 	});
 });
 </script>
 
 <template>
 	<main>
-		<h1 v-if="dbData.title">{{ dbData.title[0]?.plain_text }}</h1>
+		<h1 v-if="dbData.title">{{ dbData.title[0]?.plain_text ?? 'What the hay?!' }}</h1>
 		<p v-if="dbData.description">{{ dbData.description[0]?.text.content }}</p>
-		<section>
+		<section v-if="isLoading === false">
 			<Card
 				v-if="itemData"
 				v-for="item in itemData"
 				:title="item.properties.Name.title[0].plain_text"
-				:imgSrc="item.properties.Image.files[0]?.file.url ?? ''"
+				:subTitle="item.properties.Position.select.name"
+				:imgSrc="item.properties?.Image.files[0].file?.url"
 				:descAi="item.properties['Auto Description']?.rich_text[0]?.plain_text ?? ''"
 				:painPoints="item.properties['Pain Points (RU)'].rollup.array.map((item) => item.title[0].text.content)"
 				:unmetNeeds="item.properties['Unmet Needs (RU)'].rollup.array.map((item) => item.title[0].text.content)"
+				:roles="item.properties.Role.multi_select.map((item) => item.name)"
 				:goals="item.properties['Goals (RU)'].rollup.array.map((item) => item.title[0].text.content)"
 				:itemId="`/persona/${item.id}`"
 			/>
 		</section>
+		<Loader v-else />
 	</main>
 </template>
 <style scoped>
-h1, p {
+main {
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+}
+h1 {
+	color: var(--color-brand-100);
+}
+h1,
+p {
 	margin: 0;
 	margin-block-end: 0.5rem;
-	text-wrap: balance;
 	max-width: 65ch;
 }
 
@@ -49,12 +63,12 @@ section {
 	gap: 1rem;
 }
 @media (width <= 960px) {
-	main {
+	section {
 		grid-template-columns: repeat(2, 1fr);
 	}
 }
 @media (width <= 640px) {
-	main {
+	section {
 		grid-template-columns: repeat(1, 1fr);
 	}
 }
