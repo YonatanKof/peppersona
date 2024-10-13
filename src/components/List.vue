a6698df7dfec02e5cf5558f15a73c6d204c58cd5
Yonatan Kof <kof@yonatankof.com>
2024-10-13 20:35:14 +0300

    feat: Update List component to support dark mode and custom colors

--- src/components/List.vue
+++ src/components/List.vue
@@ -6,34 +6,51 @@ const props = defineProps({
 		type: String,
 		default: undefined,
 	},
+	darkColor: {
+		type: String,
+		default: undefined,
+	},
 });
 </script>
 
 <template>
-	<span v-if="list.length > 0">
-		<h4 id="title">{{ listName }}</h4>
-		<ul>
+	<span class="list-main" v-if="list.length > 0">
+		<h4 id="list-title">{{ listName }}</h4>
+		<ul class="list-ul">
 			<li v-for="item in list">{{ item }}</li>
 		</ul>
 	</span>
 </template>
-<style scoped>
-#title {
+<style>
+#list-title {
 	margin: 0;
-    color: v-bind(color);
+	color: v-bind(color);
+	padding-block-end: 0.5rem;
 }
-span {
+.list-main {
 	display: flex;
 	flex-direction: column;
 	gap: 0.25rem;
 	align-items: start;
+	margin-block-end: 0.5rem;
 }
-ul {
+.list-ul {
 	margin: 0;
 	padding-inline-start: 1.25rem;
-    list-style-type: circle;
+	list-style-type: circle;
 }
-li::marker {
-    color: v-bind(color);
+.list-ul > li {
+	text-wrap: balance;
+}
+.list-ul > li::marker {
+	color: v-bind(color);
+}
+@media (prefers-color-scheme: dark) {
+	#list-title {
+		color: v-bind(darkColor);
+	}
+	.list-ul > li::marker {
+		color: v-bind(darkColor);
+	}
 }
 </style>
