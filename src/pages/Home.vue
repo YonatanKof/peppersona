a86dbcb4ce65810c24a13c83e84f4402df05b042
Yonatan Kof <kof@yonatankof.com>
2024-10-13 13:55:54 +0300

    feat: Add rollup arrays for pain points, unmet needs, and goals in Card component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,6 +22,9 @@ onMounted(() => {
 			:title="item.properties.Name.title[0].plain_text"
 			:imgSrc="item.properties.Image.files[0]?.file.url ?? ''"
 			:descAi="item.properties['Auto Description']?.rich_text[0]?.plain_text ?? ''"
+			:painPoints="item.properties['Pain Points (RU)'].rollup.array.map(item => item.title[0].text.content)"
+			:unmetNeeds="item.properties['Unmet Needs (RU)'].rollup.array.map(item => item.title[0].text.content)"
+			:goals="item.properties['Goals (RU)'].rollup.array.map(item => item.title[0].text.content)"
 			:itemId="`/persona/${item.id}`"
 		/>
 	</main>
